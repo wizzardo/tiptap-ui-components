48d4352f6f5a8b8622d1e1b8dae12da2a172830c
Aslam H <iupin5212@gmail.com>
2025-04-15 00:27:08 +0700

    chore(cli): use chalk gray instead of hex

--- packages/cli/src/utils/colors.ts
+++ packages/cli/src/utils/colors.ts
@@ -2,7 +2,7 @@ import chalk from "chalk"
 
 export const colors = {
   cyan: chalk.hex("#46caff"),
-  gray: chalk.hex("#898989"),
+  gray: chalk.gray,
   magenta: chalk.hex("#f64d90"),
   white: chalk.white,
   yellow: chalk.yellow,
